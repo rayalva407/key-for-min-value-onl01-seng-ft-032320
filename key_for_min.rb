5ef539dd5ea83b5038d9f9f84ed84ad6fedf3846
Ray Alva <rayalva407@gmail.com>
2020-03-30 19:58:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  array = name_hash. do |key, value|
+  name_hash. do |key, value|
     value
   end
-  array
 end
