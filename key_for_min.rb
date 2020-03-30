3978b875dbb48b6ea6bbfe1977af77d6a0df0577
Ray Alva <rayalva407@gmail.com>
2020-03-30 20:45:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,11 +7,11 @@ def key_for_min_value(name_hash)
   end
   
   new_array = array.sort
-  value = nil
+  m = nil
   
   name_hash.each do |key, value|
     if value == nil
-      return value
+      return m
     elsif value == new_array[0]
       return key
     end
