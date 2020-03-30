fa89443cecd8f956b2ec162c1bae1f001146bf18
Ray Alva <rayalva407@gmail.com>
2020-03-30 20:46:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,9 +10,7 @@ def key_for_min_value(name_hash)
   m = nil
   
   name_hash.each do |key, value|
-    if name_hash == 0
-      return m
-    elsif value == new_array[0]
+    if value == new_array[0]
       return key
     end
   end
